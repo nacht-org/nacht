f83cdd3d80c13030532c5c59cee77e0b4a52c55f
Mensch <mensch272@users.noreply.github.com>
2022-04-25 17:45:51 +0500

    Extracted action item to its own widget.

--- lib/presentation/pages/novel_page/widgets/action_bar.dart
+++ lib/presentation/pages/novel_page/widgets/action_bar.dart
@@ -10,44 +10,61 @@ class ActionBar extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Expanded(
-            child: Material(
-              borderRadius: BorderRadius.all(Radius.circular(16)),
-              clipBehavior: Clip.antiAlias,
-              child: InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Icon(Icons.favorite),
-                      SizedBox(height: 4),
-                      Text('Add to library'),
-                    ],
-                  ),
-                ),
-              ),
+            child: ActionItem(
+              icon: Icons.favorite,
+              label: 'Add to library',
+              onTap: () {},
+              active: true,
             ),
           ),
           Expanded(
-            child: Material(
-              borderRadius: BorderRadius.all(Radius.circular(16)),
-              clipBehavior: Clip.antiAlias,
-              child: InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Icon(Icons.open_in_browser),
-                      SizedBox(height: 4),
-                      Text('WebView'),
-                    ],
-                  ),
-                ),
-              ),
+            child: ActionItem(
+              icon: Icons.open_in_browser,
+              label: 'WebView',
+              onTap: () {},
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ActionItem extends StatelessWidget {
+  const ActionItem({
+    Key? key,
+    required this.onTap,
+    required this.icon,
+    required this.label,
+    this.active = false,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+  final bool active;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final color = active ? theme.colorScheme.primary : null;
+    final textStyle = theme.textTheme.labelLarge?.copyWith(color: color);
+
+    return Material(
+      borderRadius: const BorderRadius.all(Radius.circular(16)),
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Icon(icon, color: color),
+              const SizedBox(height: 4),
+              Text(label, style: textStyle),
+            ],
+          ),
+        ),
       ),
     );
   }

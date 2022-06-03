d6e36815d7d7134572c3ac12b294e08618c6332e
Mensch <mensch272@users.noreply.github.com>
2022-06-03 05:52:31 +0500

    feat: add destination transition to more page.

--- lib/features/more/presentation/more_page.dart
+++ lib/features/more/presentation/more_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:nacht/core/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:nacht/features/home/home.dart';
 
 List<Widget> buildMoreHeader(BuildContext context, bool innerBoxIsScrolled) {
   return [
@@ -26,15 +27,17 @@ class MorePage extends StatelessWidget {
           forceElevated: innerBoxIsScrolled,
         ),
       ],
-      body: ListView(
-        padding: const EdgeInsets.all(0),
-        children: [
-          ListTile(
-            leading: const Icon(Icons.category),
-            title: const Text('Categories'),
-            onTap: () => context.router.push(const CategoryRoute()),
-          )
-        ],
+      body: DestinationTransition(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: [
+            ListTile(
+              leading: const Icon(Icons.category),
+              title: const Text('Categories'),
+              onTap: () => context.router.push(const CategoryRoute()),
+            )
+          ],
+        ),
       ),
     );
   }

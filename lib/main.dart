3a0e39967bab511beaad550fa57f6ecbcca61d45
Mensch <mensch272@users.noreply.github.com>
2022-06-10 16:49:34 +0500

    refactor: formated imports.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/foundation.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:nacht/features/splash/presentation/provider/application_provider.dart';
-import 'package:nacht/core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'nht/nht.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nacht/core/core.dart';
+import 'package:nacht/features/splash/presentation/provider/application_provider.dart';
+import 'package:nacht/nht/nht.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   // Disable http font downloads in release mode.

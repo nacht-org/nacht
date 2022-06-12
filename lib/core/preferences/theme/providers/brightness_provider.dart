f0ce6ef496a5374d6afb96e90e414a01f5999c95
Mensch <mensch272@users.noreply.github.com>
2022-06-12 16:53:43 +0500

    chore: removed unused import.

--- lib/core/preferences/theme/providers/brightness_provider.dart
+++ lib/core/preferences/theme/providers/brightness_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nacht/core/core.dart';
-import 'package:nacht/core/preferences/theme/theme.dart';
 
 final brightnessProvider = Provider<Brightness>(
   (ref) {

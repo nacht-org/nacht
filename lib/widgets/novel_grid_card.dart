994917218f2641776497a0e75209d816ec1f64d1
Mensch <mensch272@users.noreply.github.com>
2022-06-13 14:31:51 +0500

    feat: added cached image provider.

--- lib/widgets/novel_grid_card.dart
+++ lib/widgets/novel_grid_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:nacht/common/common.dart';
 
@@ -21,7 +22,7 @@ class NovelGridCard extends StatelessWidget {
     if (cover != null) {
       image = FileImage(cover!.file);
     } else if (coverUrl != null) {
-      image = NetworkImage(coverUrl!);
+      image = CachedNetworkImageProvider(coverUrl!);
     } else {
       image = null;
     }

6fc764ad5fef02d4221f9ca2d9bf206e4ed89c22
Mensch <mensch272@users.noreply.github.com>
2022-06-14 17:01:31 +0500

    refactor: home presentation.
    Refactored to match the current feature/presentation struture.

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,3 +1,2 @@
-export 'presentation/home_page.dart';
-export 'presentation/model/destinations.dart';
-export 'presentation/provider/navigation_visible_provider.dart';
+export 'presentation/pages/home_page.dart';
+export 'presentation/providers/navigation_visible_provider.dart';
---
+++ lib/features/home/presentation/presentation.dart
@@ -0,0 +1,8 @@
+// Models
+export 'models/destinations.dart';
+
+// Pages.
+export 'pages/home_page.dart';
+
+// Providers
+export 'providers/navigation_visible_provider.dart';

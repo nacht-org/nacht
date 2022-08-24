e55e7bd77d2cd572c947c8d3f4d37ed8c0acef29
Mensch <mensch272@users.noreply.github.com>
2022-08-24 20:40:33 +0500

    refactor: move set_categories into common/category.

--- lib/common/category/category.dart
+++ lib/common/category/category.dart
@@ -3,6 +3,10 @@ export 'models/category_data.dart';
 
 // Providers.
 export 'providers/categories_provider.dart';
+export 'providers/selected_categories_provider.dart';
 
 // Services.
 export 'services/watch_categories.dart';
+
+// Widgets.
+export 'widgets/set_categories_dialog.dart';
--- lib/common/novel/novel.dart
+++ lib/common/novel/novel.dart
@@ -34,7 +34,3 @@ export 'models/volume_data.dart';
 export 'providers/chapter_family.dart';
 export 'providers/chapter_list_family.dart';
 export 'providers/novel_family.dart';
-
-// Set categories
-export '../set_categories/provider/selected_categories_provider.dart';
-export '../set_categories/set_categories_dialog.dart';

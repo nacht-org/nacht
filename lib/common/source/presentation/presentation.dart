f1fafaf6aa1923d4520d0736cd1fbc2cb2faec11
Mensch <mensch272@users.noreply.github.com>
2022-08-24 20:48:21 +0500

    refactor: simplify folders in common/source.

--- lib/common/source/presentation/presentation.dart
+++
@@ -1,6 +0,0 @@
-// Models.
-export 'models/fetch_info.dart';
-export 'models/fetch_state.dart';
-
-// Widgets.
-export 'widgets/sliver_fetch_grid.dart';
--- lib/common/source/source.dart
+++ lib/common/source/source.dart
@@ -1,1 +1,6 @@
-export 'presentation/presentation.dart';
+// Models.
+export 'models/fetch_info.dart';
+export 'models/fetch_state.dart';
+
+// Widgets.
+export 'widgets/sliver_fetch_grid.dart';

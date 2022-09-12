e51f37f16282c9a7c5586cd2903ce431a313bff3
Mensch <mensch272@users.noreply.github.com>
2022-09-12 22:39:01 +0500

    feat: implement select and invert all and for singular view.

--- lib/features/library/widgets/singular_library_display.dart
+++ lib/features/library/widgets/singular_library_display.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nacht/features/features.dart';
-import 'package:nacht/features/library/providers/library_selection_provider.dart';
 import 'package:nacht/shared/shared.dart';
 import 'package:nacht/widgets/widgets.dart';
 
+import '../providers/providers.dart';
 import 'widgets.dart';
 
 class SingularLibraryDisplay extends HookConsumerWidget {
@@ -21,6 +21,7 @@ class SingularLibraryDisplay extends HookConsumerWidget {
     final controller = useNavigationScrollController(navigationNotifier);
 
     final selection = ref.watch(librarySelectionProvider);
+    final selectionNotifier = ref.watch(librarySelectionProvider.notifier);
 
     return NestedScrollView(
       controller: controller,
@@ -33,7 +34,19 @@ class SingularLibraryDisplay extends HookConsumerWidget {
             forceElevated: innerBoxIsScrolled,
           ),
         if (selection.active)
-          SliverSelectionAppBar(title: Text("${selection.selected.length}")),
+          SliverSelectionAppBar(
+            title: Text("${selection.selected.length}"),
+            onSelectAllPressed: () async {
+              final novels =
+                  await ref.read(categoryNovelsFamily(category.id).future);
+              selectionNotifier.addAll(novels.map((novel) => novel.id));
+            },
+            onInversePressed: () async {
+              final novels =
+                  await ref.read(categoryNovelsFamily(category.id).future);
+              selectionNotifier.flipAll(novels.map((novel) => novel.id));
+            },
+          ),
       ],
       body: DestinationTransition(
         child: CategoryGrid(

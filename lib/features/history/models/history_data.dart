0512793ae98ba112144db8600acff9a804973d7f
Mensch <mensch272@users.noreply.github.com>
2022-09-15 12:28:04 +0500

    feat: add history data watch service.

---
+++ lib/features/history/models/history_data.dart
@@ -0,0 +1,32 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:nacht/database/database.dart';
+import 'package:nacht/shared/shared.dart';
+
+part 'history_data.freezed.dart';
+
+@freezed
+class HistoryData with _$HistoryData {
+  const factory HistoryData({
+    required int id,
+    required DateTime addedAt,
+    required DateTime updatedAt,
+    required NovelData novel,
+    required ChapterData chapter,
+  }) = _HistoryData;
+
+  factory HistoryData.fromModel(
+    ReadingHistory model,
+    NovelData novel,
+    ChapterData chapter,
+  ) {
+    return HistoryData(
+      id: model.id,
+      addedAt: model.addedAt,
+      updatedAt: model.updatedAt,
+      novel: novel,
+      chapter: chapter,
+    );
+  }
+
+  const HistoryData._();
+}
---
+++ lib/features/history/services/watch_history.dart
@@ -0,0 +1,48 @@
+import 'package:drift/drift.dart';
+import 'package:nacht/database/database.dart';
+import 'package:nacht/shared/shared.dart';
+
+import '../models/models.dart';
+
+class WatchHistory {
+  const WatchHistory({
+    required AppDatabase database,
+  }) : _database = database;
+
+  final AppDatabase _database;
+
+  Stream<List<HistoryData>> execute() {
+    final query = _database.select(_database.readingHistories).join([
+      leftOuterJoin(
+        _database.chapters,
+        _database.readingHistories.chapterId.equalsExp(_database.chapters.id),
+      ),
+      leftOuterJoin(
+        _database.volumes,
+        _database.chapters.volumeId.equalsExp(_database.volumes.id),
+      ),
+      leftOuterJoin(
+        _database.novels,
+        _database.chapters.novelId.equalsExp(_database.novels.id),
+      ),
+    ])
+      ..orderBy([OrderingTerm.desc(_database.readingHistories.updatedAt)])
+      ..groupBy([_database.novels.id]);
+
+    return query.map((row) {
+      final history = row.readTable(_database.readingHistories);
+      final chapter = row.readTable(_database.chapters);
+      final volume = row.readTable(_database.volumes);
+      final novel = row.readTable(_database.novels);
+
+      return HistoryData.fromModel(
+        history,
+        NovelData.fromModel(novel),
+        ChapterData.fromModel(
+          chapter,
+          VolumeData.fromModel(volume),
+        ),
+      );
+    }).watch();
+  }
+}

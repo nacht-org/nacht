cee28edf747549ce239068f2898fad82fb1cd22a
Mensch <mensch272@users.noreply.github.com>
2022-09-15 14:15:38 +0500

    feat: implemented initial history tiles.

--- lib/features/history/views/history_page.dart
+++ lib/features/history/views/history_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../widgets/widgets.dart';
+
 class HistoryPage extends ConsumerWidget {
   const HistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return const Scaffold(
+      body: HistoryBody(),
+    );
   }
 }
---
+++ lib/features/history/widgets/history_body.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nacht/widgets/widgets.dart';
+
+import '../providers/history_provider.dart';
+
+class HistoryBody extends ConsumerWidget {
+  const HistoryBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final entriesValue = ref.watch(historyProvider);
+
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [],
+      body: entriesValue.when(
+        data: (entries) => ListView.builder(
+          itemBuilder: (context, index) {
+            final entry = entries[index];
+
+            return entry.when(
+              date: (date) => RelativeDateTile(date: date),
+              history: (history) => ListTile(
+                title: Text(history.novel.title),
+                subtitle: Text(history.chapter.title),
+              ),
+            );
+          },
+          itemCount: entries.length,
+        ),
+        error: (error, stack) => LoadingError(
+          message: Text(error.toString()),
+        ),
+        loading: () => const SizedBox.shrink(),
+      ),
+    );
+  }
+}

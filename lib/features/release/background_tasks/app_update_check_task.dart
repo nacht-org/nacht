55975642b68f3e6d24d4c98f5e851e7a9e43f967
Mensch <mensch272@users.noreply.github.com>
2023-04-14 01:40:28 +0500

    fix: check for internet connection before update check

--- lib/features/release/background_tasks/app_update_check_task.dart
+++ lib/features/release/background_tasks/app_update_check_task.dart
@@ -1,16 +1,23 @@
 import 'package:nacht/core/core.dart';
+import 'package:nacht/shared/shared.dart';
 import 'package:riverpod/riverpod.dart';
 
 import '../notifications/notifications.dart';
 import '../services/services.dart';
 
 class AppUpdateCheckTask extends BackgroundTask<void> with LoggerMixin {
-  static const String name = 'AppUpdateCheckTask';
+  static const String name = 'AppUpdateCheck';
 
   @override
   Future<bool> execute(ProviderContainer container, void data) async {
-    final checkResult = await container.read(checkNewReleaseProvider).call();
+    final isConnectionAvailable =
+        await container.read(getIsConnectionAvailableProvider).execute();
+    if (!isConnectionAvailable) {
+      log.info('update check cancelled: no internet connection');
+      return true;
+    }
 
+    final checkResult = await container.read(checkNewReleaseProvider).call();
     final value = checkResult.fold((failure) {
       log.warning(failure.toString());
       return false;

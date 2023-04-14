622e1b14e01fef6b216c968276946e3bba366455
Mensch <mensch272@users.noreply.github.com>
2023-04-14 13:27:01 +0500

    refactor: rename file to match class

--- lib/features/release/services/services.dart
+++ lib/features/release/services/services.dart
@@ -1,4 +1,4 @@
 export 'get_latest_release.dart';
 export 'check_new_release.dart';
-export 'get_download_link.dart';
+export 'get_platform_download_assets.dart';
 export 'check_new_release_and_route.dart';

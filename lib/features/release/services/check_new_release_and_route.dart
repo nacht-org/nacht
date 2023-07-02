afc3918c4bb40f61a1f4e36e2246834e56d9b7b7
Mensch <mensch272@users.noreply.github.com>
2023-07-02 23:49:41 +0500

    fix: check for internet connection before checking for new version

--- lib/features/release/services/check_new_release_and_route.dart
+++ lib/features/release/services/check_new_release_and_route.dart
@@ -1,4 +1,5 @@
 import 'package:nacht/core/core.dart';
+import 'package:nacht/shared/shared.dart';
 import 'package:riverpod/riverpod.dart';
 
 import 'check_new_release.dart';
@@ -22,6 +23,16 @@ class CheckNewReleaseAndRoute {
 
   Future<bool> call({bool silent = false}) async {
     final messageService = _ref.read(messageServiceProvider);
+
+    // Only continue if there is an internet connection
+    final isConnectionAvailable =
+        await _ref.read(getIsConnectionAvailableProvider).execute();
+    if (!isConnectionAvailable) {
+      if (!silent) {
+        messageService.showToast(const NoNetworkConnection().message);
+      }
+      return true;
+    }
 
     if (!silent) {
       messageService.showToast("Searching for updates...");

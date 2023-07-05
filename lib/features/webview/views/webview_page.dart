9cda9c46b8efc053745bcf75bcc8ec3594601031
Mensch <mensch272@users.noreply.github.com>
2023-07-05 18:41:05 +0500

    feat: added more interactivity to webview

--- lib/features/webview/views/webview_page.dart
+++ lib/features/webview/views/webview_page.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 @RoutePage()
@@ -15,13 +17,52 @@ class WebViewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     final controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(Uri.parse(initialUrl));
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title),
+            Text(
+              initialUrl,
+              style: theme.textTheme.titleSmall,
+            ),
+          ],
+        ),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: const Text('Refresh'),
+                onTap: () => controller.reload(),
+              ),
+              PopupMenuItem(
+                child: const Text('Share'),
+                onTap: () async {
+                  final url = await controller.currentUrl();
+                  if (url != null) {
+                    Share.share(url);
+                  }
+                },
+              ),
+              PopupMenuItem(
+                child: const Text('Open in browser'),
+                onTap: () async {
+                  final url = await controller.currentUrl();
+                  if (url != null) {
+                    launchUrlString(url, mode: LaunchMode.externalApplication);
+                  }
+                },
+              ),
+            ],
+          )
+        ],
       ),
       body: WebViewWidget(
         controller: controller,

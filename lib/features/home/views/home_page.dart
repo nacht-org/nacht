4b416ec5c51bf9bd00bd35d6cd196624fde4c05d
Mensch <mensch272@users.noreply.github.com>
2023-07-07 16:08:58 +0500

    fix: changing navigation index when trying to exit from selection route

--- lib/features/home/views/home_page.dart
+++ lib/features/home/views/home_page.dart
@@ -15,65 +15,78 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    const initialIndex = 0;
     final currentIndex = useState(0);
 
     usePostFrameCallback((timeStamp) {
       ref.read(applicationLoadedProvider).init();
     });
 
-    return LazyIndexedStack(
-      initialIndex: 0,
-      currentIndex: currentIndex.value,
-      destinations: destinations.map((item) => item.builder).toList(),
-      duration: kShortAnimationDuration,
-      builder: (context, child, animation) {
-        return Scaffold(
-          body: FadeTransition(
-            opacity: animation,
-            child: MediaQuery(
-              data: MediaQuery.of(context),
-              child: child,
-            ),
-          ),
-          extendBody: true,
-          bottomNavigationBar: HookConsumer(
-            builder: (context, ref, child) {
-              final controller = useAnimationController(
-                initialValue: 1,
-                duration: kShortAnimationDuration,
-              );
-
-              ref.listen<NavigationInfo>(
-                navigationProvider,
-                (prev, next) {
-                  final prevForceHide = prev?.forceHide ?? false;
-                  if (next.forceHide && !prevForceHide) {
-                    controller.reverse();
-                  } else if (!next.forceHide && prevForceHide) {
-                    controller.forward();
-                  }
-                },
-              );
-
-              return AnimatedBottomBar(
-                controller: controller,
-                child: NavigationBar(
-                  selectedIndex: currentIndex.value,
-                  onDestinationSelected: (index) => currentIndex.value = index,
-                  destinations: List.generate(destinations.length, (index) {
-                    final destination = destinations[index];
-                    return NavigationDestination(
-                      icon: destination.icon,
-                      selectedIcon: destination.selectedIcon,
-                      label: destination.label,
-                    );
-                  }),
-                ),
-              );
-            },
-          ),
-        );
+    return WillPopScope(
+      onWillPop: () async {
+        final hasModalRoute =
+            ModalRoute.of(context)?.willHandlePopInternally ?? false;
+        if (!hasModalRoute && currentIndex.value != initialIndex) {
+          currentIndex.value = initialIndex;
+          return false;
+        }
+        return true;
       },
+      child: LazyIndexedStack(
+        initialIndex: initialIndex,
+        currentIndex: currentIndex.value,
+        destinations: destinations.map((item) => item.builder).toList(),
+        duration: kShortAnimationDuration,
+        builder: (context, child, animation) {
+          return Scaffold(
+            body: FadeTransition(
+              opacity: animation,
+              child: MediaQuery(
+                data: MediaQuery.of(context),
+                child: child,
+              ),
+            ),
+            extendBody: true,
+            bottomNavigationBar: HookConsumer(
+              builder: (context, ref, child) {
+                final controller = useAnimationController(
+                  initialValue: 1,
+                  duration: kShortAnimationDuration,
+                );
+
+                ref.listen<NavigationInfo>(
+                  navigationProvider,
+                  (prev, next) {
+                    final prevForceHide = prev?.forceHide ?? false;
+                    if (next.forceHide && !prevForceHide) {
+                      controller.reverse();
+                    } else if (!next.forceHide && prevForceHide) {
+                      controller.forward();
+                    }
+                  },
+                );
+
+                return AnimatedBottomBar(
+                  controller: controller,
+                  child: NavigationBar(
+                    selectedIndex: currentIndex.value,
+                    onDestinationSelected: (index) =>
+                        currentIndex.value = index,
+                    destinations: List.generate(destinations.length, (index) {
+                      final destination = destinations[index];
+                      return NavigationDestination(
+                        icon: destination.icon,
+                        selectedIcon: destination.selectedIcon,
+                        label: destination.label,
+                      );
+                    }),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }

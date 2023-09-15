ccef9e4076cd0eff852c9f7500d5c7d0d7fbdd27
Mensch <mensch272@users.noreply.github.com>
2023-09-15 10:41:47 +0500

    feat: added logo to crawler list

--- lib/features/browse/main/widgets/sliver_crawler_list.dart
+++ lib/features/browse/main/widgets/sliver_crawler_list.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nacht/core/core.dart';
+import 'package:nacht_sources/nacht_sources.dart';
 
 import '../providers/providers.dart';
 
@@ -29,6 +33,10 @@ class SliverCrawlerList extends ConsumerWidget {
             );
           }, crawler: (entry) {
             return ListTile(
+              leading: CrawlerLogo(
+                meta: entry.meta,
+                isSupported: entry.isSupported,
+              ),
               title: Text(entry.meta.name),
               subtitle: Text(entry.meta.version.version),
               onTap: () => context.router.push(
@@ -39,6 +47,53 @@ class SliverCrawlerList extends ConsumerWidget {
           });
         },
         childCount: crawlers.length,
+      ),
+    );
+  }
+}
+
+class CrawlerLogo extends StatelessWidget {
+  const CrawlerLogo({
+    super.key,
+    required this.meta,
+    required this.isSupported,
+  });
+
+  final Meta meta;
+  final bool isSupported;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child = CachedNetworkImage(
+      imageUrl: meta.logo.src,
+      placeholder: (context, url) => DecoratedBox(
+        decoration: BoxDecoration(
+          color: Color(meta.logo.color),
+        ),
+      ),
+      errorWidget: (context, url, error) => DecoratedBox(
+        decoration: BoxDecoration(
+          color: Color(meta.logo.color),
+        ),
+      ),
+      fit: BoxFit.cover,
+    );
+
+    if (!isSupported) {
+      child = ColorFiltered(
+        colorFilter: const ColorFilter.mode(
+          Colors.grey,
+          BlendMode.saturation,
+        ),
+        child: child,
+      );
+    }
+
+    return SizedBox.square(
+      dimension: 40,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12.0),
+        child: child,
       ),
     );
   }
